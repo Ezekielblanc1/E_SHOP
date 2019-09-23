c3ec323b29380332dacb6be1d4a0ff67b15e2ee0
Ezekiel <ezekielblanc1@gmail.com>
2019-09-23 16:24:31 +0100

    Imported switch from react router dom

--- e_commerce/src/App.js
+++ e_commerce/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import HomePage from '../src/components/pages/homepage/homepage.component'
 import ShopPage from '../src/components/pages/shop_page/shop_page.component'
+import Header from './components/header/header.component'
 
 
 
 function App() {
  return (
    <div>
+      <Header/>
+      <Switch>
        <Route exact path = '/' component = {HomePage}/> 
        <Route path = '/shop' component = {ShopPage}/>
+      </Switch>
    </div>
  )
 }
